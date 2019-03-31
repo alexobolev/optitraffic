c4998b2f44d7f8472347f8168e17644e91a5115c
Alex Sobolev <finalso23@gmail.com>
2019-03-31 19:26:11 +0300

    Started implementing code, API study

---
+++ optitraffic/Models/TmsData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace optitraffic.Models
+{
+    public class TmsData
+    {
+        public int StationId { get; set; }
+
+    }
+}
---
+++ optitraffic/Models/TmsStation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace optitraffic.Models
+{
+    public class TmsStation
+    {
+        public int Id { get; }
+        public int MunicipalityCode { get; set; }
+        public string MunicipalityName { get; set; }
+        public List<int> StationSensorsCodes { get; }
+
+        public TmsStation() : this(-1, -1, null)
+        {
+
+        }
+
+        public TmsStation(int id, int mun_code, string mun_name)
+        {
+            this.Id = id;
+            this.MunicipalityCode = mun_code;
+            this.MunicipalityName = mun_name;
+            this.StationSensorsCodes = new List<int>();
+        }
+
+        public void AddStationSensorCode(int code)
+        {
+            this.StationSensorsCodes.Add(code);
+        }
+    }
+}
